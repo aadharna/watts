9f8eec3276cfa2df084e25f3207980fa06a76746
aadharna <aadharna@gmail.com>
2021-03-14 17:52:50 -0400

    initial pair class. This holds the NN and a generator for levels for the env. This does NOT hold the env itself

---
+++ pair/agent_environment_pair.py
@@ -0,0 +1,41 @@
+from utils.loader import load_from_yaml
+
+from generators.base import BaseGenerator
+from ray.rllib.agents.trainer import Trainer
+
+class Pair:
+    id = 0
+
+    def __init__(self, file_args, agent: Trainer, generator: BaseGenerator):
+
+        self.args = file_args
+
+        self.agent = agent
+        self.generator = generator
+
+        self.id = Pair.id
+        Pair.id += 1
+
+    def __str__(self):
+        return str(self.generator)
+
+    # def _evaluate(self) -> float:
+    #     """
+    #     The generator at this time has a level embedded in it already.
+    #     Evaluate the attached NN in this defined map.
+    #     :return: float
+    #     """
+    #     resultDict = self.agent._evaluate()
+    #     return resultDict['episode_reward_mean']
+    #
+    # def _train(self):
+    #     """
+    #
+    #     :return:
+    #     """
+    #     ptr = self.agent.train()
+    #     return ptr
+
+
+if __name__ == "__main__":
+    pass
