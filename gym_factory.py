5a7209ef49e482b7c7ceefe679f6d52621cbaf6f
aadharna <aadharna@gmail.com>
2021-03-18 23:43:26 -0400

    remove file_args from cnstr; pass in env_config to make; add type-hinting

--- gym_factory.py
+++ gym_factory.py
@@ -1,31 +1,31 @@
 from griddly.util.rllib.wrappers.core import RLlibEnv
 from ray.tune.registry import register_env
 
+
 class GridGameFactory:
-    def __init__(self, file_args, env_wrappers: list, registrar):
+    def __init__(self, registrar, env_wrappers: list):
         """Factory to create new gym envs and register it with ray's global env register
 
         :param registrar: utils.registry.Registrar class. This holds info needed to make new envs
-        :param file_args: arguments loaded from file via utils.loader.load_yaml_file
         :param env_wrappers: list of env.Wrappers to apply to the env
         """
 
         self.registrar = registrar
-        self.args = file_args
+        self.args = self.registrar.file_args
         self.env_wrappers = env_wrappers
         register_env(self.registrar.name, self.make())
 
     def make(self):
-        def _make(env_config: dict = dict()):
+        def _make(env_config: dict = dict()) -> RLlibEnv:  # this RLlibEnv is (potentially) wrapped.
             """function used to register env creation with rllib.
 
             :param env_config: unused param. Here for compatibility purposes with rllib
             :return: (wrapped) RLlibEnv from griddly
             """
-            env = RLlibEnv(self.registrar.get_config_to_build_rllib_env)
+            env = RLlibEnv(env_config)
             env.enable_history(True)
             for i, wrapper in enumerate(self.env_wrappers):
-                env = wrapper(env, self.registrar.get_config_to_build_rllib_env)
+                env = wrapper(env, env_config)
             return env
         return _make
 
@@ -40,7 +40,7 @@ if __name__ == "__main__":
 
     registry = Registrar(file_args=args)
 
-    gameFactory = GridGameFactory(file_args=args, env_wrappers=[AlignedReward], registrar=registry)
+    gameFactory = GridGameFactory(registrar=registry, env_wrappers=[AlignedReward])
 
     env = gameFactory.make()()#registry.get_config_to_build_rllib_env)
     import matplotlib.pyplot as plt
