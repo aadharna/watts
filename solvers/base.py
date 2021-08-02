67ebd7d7025ecec0b3644fad32280aa0fcaffbcf
aadharna <aadharna@gmail.com>
2021-08-02 15:06:42 -0400

    update fn call sig

--- solvers/base.py
+++ solvers/base.py
@@ -5,7 +5,7 @@ class BaseSolver:
         self.id = BaseSolver.id
         BaseSolver.id += 1
 
-    def evaluate(self, env) -> dict:
+    def evaluate(self, env_generator_fn, env_config) -> dict:
         raise NotImplementedError
 
     def optimize(self, trainer_config, level_string_monad, **kwargs):
