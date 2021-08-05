f01a11e5d0ab2d153a8c841b5a90089f52460fe5
aadharna <aadharna@gmail.com>
2021-08-04 21:07:18 -0400

    update rollout fn call in test

--- tests/evaluators/evaluate_tests.py
+++ tests/evaluators/evaluate_tests.py
@@ -4,6 +4,7 @@ import network_factory
 import gym_factory
 import tests.test_structs as test_structs
 import unittest
+import os
 
 
 class TestEvaluate(unittest.TestCase):
@@ -11,7 +12,7 @@ class TestEvaluate(unittest.TestCase):
     def test_evaluate(self):
         gf = gym_factory.GridGameFactory("foo", [])
         rllib_env_config = {
-            'yaml_file': 'levels/limited_zelda.yaml',
+            'yaml_file': os.path.join('levels', 'limited_zelda.yaml'),
             'level_string': 'wwwwwwwwwwwww\nw....+e.....w\nw...........w\nw..A........w\nw...........w\nw...........w\nw.....w.....w\nw.g.........w\nwwwwwwwwwwwww\n'
         }
         env = gf.make()(rllib_env_config)
@@ -21,7 +22,7 @@ class TestEvaluate(unittest.TestCase):
         nf = network_factory.NetworkFactory(network_factory.aiide, build_info)
         actor = nf.make()(test_structs.example_aiide_state_dict)
 
-        info, states, actions, rewards, win, logps, entropies = rollout(actor, env)
+        info, states, actions, rewards, win, logps, entropies = rollout(actor, env, 'cpu')
         print(info)
         print(rewards)
         print(sum(rewards))
