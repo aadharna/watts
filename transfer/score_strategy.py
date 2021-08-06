528c0f448ab48dcb278922173e77403ef2557971
aadharna <aadharna@gmail.com>
2021-08-06 01:42:44 -0400

    remove todo

--- transfer/score_strategy.py
+++ transfer/score_strategy.py
@@ -14,7 +14,6 @@ class ZeroShotCartesian(ScoreStrategy):
         """run the evaluate function on the cartesian product of solvers and generators
 
         :param solvers: list of Solver objects
-        #TODO: make this a callable that will generate the string via e.g. NN-weights for a generator network
         :param generators: list of callable string levels
         :param id_map: list of tuples of (solver_id, generator_id) also created with product (id, id)
         :return: list of refs (and list of ids) for the evaluated solver-generator pairs
@@ -22,8 +21,6 @@ class ZeroShotCartesian(ScoreStrategy):
 
         refs = []
         for i, (s, g) in enumerate(product(solvers, generators)):
-            # todo: I don't like this. We need a better way of getting the solver/generator ids.
-            # this is the same to do as below.
             solver_id, generator_id = id_map[i]
             self.env_config['level_string'], _ = g.generate_fn_wrapper()()
             refs.append(s.evaluate.remote(env_config=self.env_config,
