6cd6b87cbd842314d4cde8c7952336ff1652b5b2
Rohin Dasari <rohin.dasari@gmail.com>
2021-11-02 20:23:55 -0400

    rebased with master

--- tests/validators/level_validator_tests.py
+++ tests/validators/level_validator_tests.py
@@ -1,7 +1,8 @@
 import os
 import ray
 import unittest
-
+import sys
+sys.path.append('../../')
 import network_factory
 from generators.PCGRLGenerator import PCGRLGenerator
 from generators.static_generator import StaticGenerator
@@ -15,7 +16,7 @@ class TestLevelValidators(unittest.TestCase):
 
     def test_graph_validator_on_PCGRL(self):
         build_info = example_pcgrl_network_factory_build_info
-        build_info['name'] = 'pcgrl_test'
+        build_info['name'] = 'Adversarial_PCGRL'
         generator = PCGRLGenerator(**build_info)
         validator = GraphValidator()
         res = validator.validate_level([generator], [MockSolver()])
@@ -30,9 +31,9 @@ class TestLevelValidators(unittest.TestCase):
 
     def test_random_agent_validator_on_static(self):
         build_info = example_network_factory_build_info
-        build_info['name'] = 'conv_test'
+        build_info['network_name'] = 'SimpleConvAgent'
         level_string = '''wwwwwwwwwwwww\nw....+e.....w\nw...........w\nw..A........w\nw...........w\nw...........w\nw.....w.....w\nw.g.........w\nwwwwwwwwwwwww\n'''
-        nf = network_factory.NetworkFactory(network_name=network_factory.conv, nn_build_info=build_info)
+        nf = network_factory.NetworkFactory(build_info)
         generator = StaticGenerator(level_string=level_string)
         validator = RandomAgentValidator(network_factory_monad=nf.make(),
                                          env_config={'yaml_file': os.path.join('levels', 'limited_zelda.yaml')})
@@ -40,3 +41,7 @@ class TestLevelValidators(unittest.TestCase):
         # therefore we cannot call ray.get on the output from `get_weights`
         # with self.assertRaises(ValueError):
         #     res = validator.validate_level([generator], [MockSolver()])
+
+if __name__ == '__main__':
+    unittest.main()
+
