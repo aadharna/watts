7c2ef13246010cc4975601f43832aa402733fde1
Rohin Dasari <rohin.dasari@gmail.com>
2022-01-03 18:53:51 -0600

    fixed Charlie's email

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='watts',
     description='A framework for exploring open-endedness in reinforcement learning',
-    author='Aaron Dharna <aadharna@gmail.com>, Charlie Summers <charlie@gomerits.com>, Rohin Dasari <rd2893@nyu.edu>',
+    author='Aaron Dharna <aadharna@gmail.com>, Charlie Summers <cgs2161@columbia.edu>, Rohin Dasari <rd2893@nyu.edu>',
     url='https://github.com/aadharna/watts',
     packages=['watts'],
     python_requires='>=3.7.10',
