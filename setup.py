8c1ce438acb7d707778d715e94ea296088cf2fc4
Rohin Dasari <rohin.dasari@gmail.com>
2022-01-03 21:44:48 -0600

    added version to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 
 setup(
     name='watts',
+    version='0.0.1',
     description='A framework for exploring open-endedness in reinforcement learning',
     author='Aaron Dharna <aadharna@gmail.com>, Charlie Summers <cgs2161@columbia.edu>, Rohin Dasari <rd2893@nyu.edu>',
     url='https://github.com/aadharna/watts',
