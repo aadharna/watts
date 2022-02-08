286cb77c62543d9b875ee272532b10ee6b56053a
aadharna <aadharna@gmail.com>
2022-02-07 21:02:48 -0500

    make it so that you can save to disk; loading back the serialized form is proving tricky. This also allows for saving the archive_dict which contains the weights and all that shit. Saving via the POETSerializer is supposed to make it so that we can pick back up the algorithm from a saved position. These are two related but different things. This change fixes the save the archive and save via the POETserializer. But we cannot load from the Serializer yet

--- watts/serializer/POETManagerSerializer.py
+++ watts/serializer/POETManagerSerializer.py
@@ -6,7 +6,7 @@ from ray.rllib.utils import add_mixins
 
 from ..managers import POETManager
 from ..utils.trainer_reset import ResetConfigOverride
-from ..utils.register import get_default_trainer_config_and_constructor, Registrar
+from ..utils.register import get_default_trainer_config_constructor_and_policy_fn, Registrar
 from ..solvers.SingleAgentSolver import SingleAgentSolver
 
 
@@ -28,10 +28,12 @@ class POETManagerSerializer():
 
         # Take out registrar's unserializable trainer_constr - we have the information to recreate it from file_args
         delattr(manager.registrar, 'trainer_constr')
+        delattr(manager.registrar, 'policy_class')
+        delattr(manager.network_factory, 'policy_class')
 
         # Take out unserializable solver and replace it with the state necessary to recreate it
         for pair in manager.active_population:
-            pair.solver_state = ray.get(pair.solver.get_picklable_state.remote())
+            pair.solver_state = pair.get_picklable_state()['solver']
             delattr(pair, 'solver')
 
         attributes = self.__dict__
@@ -42,17 +44,20 @@ class POETManagerSerializer():
         self.__dict__ = state
 
         # Recreate trainer_constr in the registrar
-        _, trainer_constr = get_default_trainer_config_and_constructor(self.manager.registrar.file_args.opt_algo)
+        _, trainer_constr, policy_class_fn = get_default_trainer_config_constructor_and_policy_fn(self.manager.registrar.file_args.opt_algo)
         if self.manager.registrar.file_args.custom_trainer_config_override:
             trainer_constr = add_mixins(trainer_constr, [ResetConfigOverride])
         self.manager.registrar.trainer_constr = trainer_constr
+        self.manager.registrar.policy_class = policy_class_fn(self.manager.registrar.get_trainer_config)
 
         # Recreate the solvers
         for pair in self.manager.active_population:
+            nf = pair.solver_state['network_factory']
+            nf.policy_class = self.manager.registrar.policy_class
             pair.solver = SingleAgentSolver.remote(trainer_constructor=trainer_constr,
                                                    trainer_config=pair.solver_state['trainer_config'],
                                                    registered_gym_name=pair.solver_state['registered_gym_name'],
-                                                   network_factory=pair.solver_state['network_factory'],
+                                                   network_factory=nf,
                                                    gym_factory=pair.solver_state['gym_factory'],
                                                    weights=pair.solver_state['weights'],
                                                    log_id=f"{pair.solver_state['log_id']}")
